8754ed7cf7c8b2a449a4c9976b4850d9cd121e83
AtiqurRahmanLabib <atiqurrahmanlabib@gmail.com>
2025-03-03 03:30:22 +0600

    Regular expression validation and toggle show password

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,17 +1,25 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
 import auth from '../../Firebase/firebase.init';
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [resgistredError, setResgistred] = useState('');
     const [success, setSuccess] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleSignUp = (e) => {
         e.preventDefault();
         if (password.length < 6) {
             setResgistred('Password must be at least 6 characters long');
+            return;
+        }
+        else if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setResgistred('Password must contain at least two upper case letters');
             return;
         }
         setResgistred('');
@@ -50,13 +58,16 @@ const SignUp = () => {
                             Password
                         </label>
                         <input
-                            type="password"
+                            type={showPassword ? "password" : "text"}
                             id="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             required
                             className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                         />
+                        <button className='mt-5' onClick={() => setShowPassword(!showPassword)}><span>
+                            {showPassword ? <FaEyeSlash /> : <FaEye />}
+                        </span></button>
                     </div>
                     <button
                         type="submit"
